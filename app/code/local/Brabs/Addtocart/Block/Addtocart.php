acc2fd03569aba99a0d90699a9d0d032a7ede4c2
Chris Brabender <cbrabender@gmail.com>
2013-12-29 22:13:47 +1100

    Version 0.1
    Initial commit for the fixed add to cart extension.

---
+++ app/code/local/Brabs/Addtocart/Block/Addtocart.php
@@ -0,0 +1,11 @@
+<?php
+
+class Brabs_Addtocart_Block_Addtocart extends Mage_Core_Block_Template
+{
+
+	public function _prepareLayout()
+	{
+		return parent::_prepareLayout();
+	}
+
+}
---
+++ app/code/local/Brabs/Addtocart/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Snipperman
+ * Date: 24/12/13
+ * Time: 7:04 PM
+ */ 
+class Brabs_Addtocart_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
